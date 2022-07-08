8c40a87609ce7b4b288127d606c0613138422d21
Chang Liu <lc12251109@gmail.com>
2022-07-08 11:20:01 -0700

    Fix eslint error
    Signed-off-by: Chang Liu <lc12251109@gmail.com>

--- src/plugins/permission_management/server/routes/permission_evaluation.ts
+++ src/plugins/permission_management/server/routes/permission_evaluation.ts
@@ -14,9 +14,9 @@ export function evaluatePermissionRoute(router: IRouter): void {
       const axiosResponse: AxiosResponse = await axios.get(
         `http://localhost:5601/api/saved_objects/_find?type=sample-permission`
       );
-      const savedObjects = axiosResponse.data.saved_objects
-      let attributes = savedObjects.map(({ attributes }) => attributes);
-      let permissions = attributes.map(({ name }) => name);
+      const savedObjects = axiosResponse.data.saved_objects;
+      const attributes = savedObjects.map(({ attributes }) => attributes);
+      const permissions = attributes.map(({ name }) => name);
       const result = permissions.includes(permissionRequired);
       return response.ok({
         body: {
